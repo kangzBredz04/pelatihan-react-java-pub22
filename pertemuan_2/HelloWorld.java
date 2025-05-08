fdbf44f52bfc117198e946159d9807a7c508c544
Wahyu <wabredz1234@gmail.com>
2025-05-08 15:03:01 +0700

    Adds Hello World program
    Adds a basic Hello World program as a starting point.
    
    This initial commit sets up the project structure and includes a
    simple program that prints "Hello, world!" to the console. It also
    demonstrates package declaration, library import, class definition,
    and a main method.

---
+++ pertemuan_2/HelloWorld.java
@@ -0,0 +1,9 @@
+package pertemuan_2; // 1.1.1 Deklarasi Package
+
+import java.util.Scanner; // 1.1.2 Impor Library
+
+public class HelloWorld { // 1.1.3 Bagian Class
+    public static void main(String[] args) { // 1.1.4 Method Main
+        System.out.println("Hello, world!"); // 1.1.5 Gaya Penulisan Case
+    }
+}
